b2b579315bce1760e22d9d32c6b3a879657e5968
Tim <tim.bathgate@gmail.com>
2017-01-05 11:31:11 +1300

    Write outline for functional test and scaffold unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_edit_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes to
+        # check out its homepage
+        self.browser.get('http://localhost:8000')
+        self.browser.implicitly_wait(3)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box
+
+        # When she hits enter, the page updates and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text vox inviting her to add another item. She
+        # enters "Use peackcock feathers to make a fly"
+
+        # The page updates again, and no shows both items
+
+        # Edit wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect
+
+        # She visits the URL - her to-do list is still there
+
+        # Satisified, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
